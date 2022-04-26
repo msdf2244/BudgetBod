8f8cc02e4dc237276adc4dc783898bd8bf664d51
msdf2244 <64487939+msdf2244@users.noreply.github.com>
2022-04-26 00:31:21 -0500

    Getting JSON through POST request

--- index.js
+++ index.js
@@ -3,18 +3,17 @@ const app = express()
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname));
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }))
+
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 })
 
 app.post("/", (req, res) => {
     console.log("POST request sent");
-    console.log(req.body.fname)
-    console.log(req.body);
-
+    
 })
 
 app.listen(3000, () => {
