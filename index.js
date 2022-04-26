256ddc8c4352150a17c984ef77627786f7882bf0
msdf2244 <64487939+msdf2244@users.noreply.github.com>
2022-04-26 00:06:08 -0500

    Changed form to do a post request

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require("express")
 const app = express()
- 
-app.use(express.static(__dirname));
 
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname));
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }))
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html")
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/", (req, res) => {
+    console.log("POST request sent");
+    console.log(req.body.fname)
+    console.log(req.body);
+
 })
 
 app.listen(3000, () => {
-    console.log("http://localhost:3000/");
-  });
+    console.log("listening on port 3000 -> http://localhost:3000/");
+});
