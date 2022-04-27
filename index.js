805c8cae7403ca1c10c7ddd2816e28d5b5b8561f
msdf2244 <64487939+msdf2244@users.noreply.github.com>
2022-04-27 03:51:32 -0500

    got recipe cards working

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const app = express()
+const recipes = require("./recipes.json")
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname));
@@ -36,12 +37,48 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 })
 
-app.post("/", (req, res) => {
-    console.log("POST request sent");
-    let gender = req.body.gender === "male";
-    let bmr = basal_metabolic_rate()
-    // var calories = caloric_intake(basal_metabolic_rate())
+async function filter_recipes(recommended_calories, budget) {
+    let MAX_RECIPES = 10;
+    let selected_recipes = []
+    let count = 0;
+    console.log(Object.keys(recipes));
+    for (var key of Object.keys(recipes)) {
+        let current = {}
+        console.log(recipes[key]);
+        let cal = recipes[key].calories;
+        let price = recipes[key].price;
+        console.log(cal, price);
+        if (cal < recommended_calories && price < budget) {
+            current["name"] = recipes[key].name;
+            current["calories"] = recipes[key].calories;
+            current["img_path"] = recipes[key].img_path;
+            selected_recipes.push(current);
+            count++;
+        }
+        if (count === MAX_RECIPES) {
+            break;
+        }
+    }
+    return selected_recipes
+}
+
+app.post("/", async (req, res) => {
+    let is_male = req.body.gender === "Male";
+    let height = parseInt(req.body.inches) + (parseInt(req.body.feet) * 12);
+    let weight = parseInt(req.body.weight);
+    let age = parseInt(req.body.age);
+
+    let bmr = basal_metabolic_rate(is_male, height, weight, age);
+    let calories = caloric_intake(bmr, parseInt(req.body.activity_level));
+    let recommended_calories = calories / 3;
+    let budget = parseInt(req.body.budget) / 90;
+    let filtered_recipes = await filter_recipes(recommended_calories, budget);
+    res.render("./recipes.ejs", {recipes: filtered_recipes});
 })
+
+app.post("/recipe_selection", (req, res) => {
+    console.log("recipe selection POSTED");
+});
 
 app.listen(3000, () => {
     console.log("listening on port 3000 -> http://localhost:3000/");
